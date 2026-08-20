89003e90b613ddb8beb2d657bf199f888bcd60ac
CM1842 <collinchen1206@gmail.com>
2026-08-20 14:49:27 -0700

    Create Hello.cpp
    test

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,14 @@
+/***
+ * {Short Description of the Lab}
+ ***/
+
+ #include <iostream>
+ #include <cstdlib>
+using namespace std;
+
+ int main()
+ {
+    cout << "Hellow World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+ }

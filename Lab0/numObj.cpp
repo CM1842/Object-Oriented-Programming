8806662fefacb4db3e332ead51d2ffda36f2d127
CM1842 <collinchen1206@gmail.com>
2026-08-27 10:29:01 -0700

    2

--- Lab0/numObj.cpp
+++ Lab0/numObj.cpp
@@ -12,12 +12,27 @@ int main()
     //non-numerical
     char char_example = 'a';
     bool bool_example = true;
+
+    //array - data structure
+    //-holds data that are the same type (int, string, class obj)
+    //-fixed size
+    //Index addressable ex. Arr [32, 1, 7 , 9] Index: 0,1,2,3 --> Arr[0] = 32, Arr[1] = 1
+
+    //array ex
+    const int SIZE = 10;
+    int arr[SIZE];
+
     
+    //8 bits = 1 Byte
     cout << "int_example: " << sizeof(int_example) << endl;
     cout << "float_example: " << sizeof(float_example) << endl;
     cout << "double_example: " << sizeof(double_example) << endl;
     cout << "char_example: " << sizeof(char_example) << endl;
     cout << "bool_example: " << sizeof(bool_example) << endl;
+    cout << "arr_int_example: " << sizeof(arr) << endl;
+    cout << "arr[0]: " << arr[0] << endl;
+    cout << "arr[1]: " << arr[1] << endl;
+    
     return 0;
 
 }
